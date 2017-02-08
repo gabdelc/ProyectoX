d603ddefd2d21cf21dbd0538f1009299e06d311f
gabriela <gabdelc@gmail.com>
2017-02-08 11:29:34 -0500

    funciones

--- js/main.js
+++ js/main.js
@@ -1,84 +1,135 @@
-// LOGICA 
-
-
-// VISUAL
-
-function onMensajeKey(evt)
+//Lógica
+function Chat(_nombre, _imagen)
 {
-   if(evt.keyCode==13)
-    {
- var elinputMensajes= document.getElementById("mensajes");    crearMensaje(elinputMensajes.value);
-    crearChat();
-        elinputMensajes.value="";
+	this.nombre =  _nombre;
+	this.imagenURL = _imagen;
+	this.ultimoMensaje = "";
+	this.horaUltimoMensaje = '';
+}
+
+var dataListaChats = [
+    new Chat("Rodulfo Prieto", 'image/rodulfo.jpg'),
+    new Chat("Raymi Saldomando", 'image/raymi.jpg'),	
+	new Chat("Ana María Martinez Franklin", 'image/anamaria.jpg'),	
+    new Chat("Mariana Costa", 'image/mariana.jpg'),
+];
+
+
+//Parte visual
+var liListItem = null;
+
+function init() {
+
+	initChatList();
+}
+
+function initChatList() {
+	var elListaChats = document.getElementById("lista-chats");
+
+	for (var i in dataListaChats) {
+		var htmlChatItem = '<li><div class="avatar">' +
+			'<img src="' + dataListaChats[i].imagenURL +  '" alt="" class="wh-44">' +
+			'<h4 class="w-contact-name">' + dataListaChats[i].nombre + '</h4>' +
+			'<p class="w-last-message" id="mensaje">' + dataListaChats[i].ultimoMensaje + '</p>' +
+			'</div>' +
+			'<div class="time" id="hora">' + dataListaChats[i].horaUltimoMensaje + '</div></li>';
+		//dataListaChats[i].borrarMensajes();
+		elListaChats.innerHTML += htmlChatItem;
+	}
+
+	setEventsChatList();
+}
+
+function setEventsChatList() {
+	var listaChats = document.getElementById('lista-chats');
+	var arrListItems = listaChats.getElementsByTagName('li');
+
+	for (var i = 0; i < arrListItems.length; i++) {
+		
+		arrListItems[i].addEventListener('click', onChatItemClick);
+	}
+}
+
+function onChatItemClick(evt) {
+	var contactName = evt.currentTarget.getElementsByClassName('w-contact-name')[0].textContent;
+	var imgURL = evt.currentTarget.getElementsByClassName('wh-44')[0].src;
+	console.log('click');
+    
+	actualizarCabeceraChat(contactName, imgURL, "Conectado");
+    borrarMensajes();
+}
+
+function onMensajeKey(evt) {
+	if (evt.keyCode == 13) {
+    
+    var elInputMensajes = document.getElementById("mensajes");
+
+		crearChat(elInputMensajes.value);
+		crearMensaje(elInputMensajes.value);
+
+		elInputMensajes.value = "";
+	}
+}
+
+function crearMensaje(_mensaje) {
+	var htmlMensajeIn = '<div class="w-message w-message-in">' +
+		'<div class="w-message-text">' +
+		'<h5 class="green-1">Maria Paula Rivarola</h5>' +
+		'<p>Jajaja Sii finalmente se corto!!</p>' +
+		'<div class="time">11:13</div>' +
+		'</div>' +
+		'</div>';
+
+	var d = new Date();
+	var htmlMensajeOut = '<div class="w-message w-message-out">' +
+		'<div class="w-message-text">' +
+		'<p>' + _mensaje + '</p>' +
+		'<div class="time">' + d.getHours() + ':' + d.getMinutes();
+	+'</div>' +
+	'</div>' +
+	'</div>';
+
+	var mensaje = liListItem.getElementsByClassName("w-last-message")[0];
+	mensaje.innerHTML = _mensaje;
+
+
+	var elChat = document.getElementById("chat");
+	elChat.innerHTML += htmlMensajeOut;
+	elChat.scrollTop = elChat.scrollHeight;
+}
+
+
+
+function crearChat(_mensaje) {
+	var elListaChats = document.getElementById("lista-chats");
+
+	if (liListItem == null) {
+		liListItem = document.createElement('LI');
+		var htmlChatItem = '<div class="avatar">' +
+			'<img src="image/logocodeacademy.png" alt="" class="wh-44">' +
+			'<h4 class="w-contact-name">Laboratoria Perú</h4>' +
+			'<p class="w-last-message" id="mensaje">' + _mensaje + '</p>' +
+			'</div>' +
+			'<div class="time" id="hora">14:27</div>';
+
+		liListItem.innerHTML = htmlChatItem;
+
+		elListaChats.insertBefore(liListItem, elListaChats.childNodes[0]);
+	}
+	setEventsChatList();
+}
+
+function actualizarCabeceraChat(_contactName, _imageURL, _estado) {
+	var chatHeader = document.getElementById("chat-header");
+	chatHeader.getElementsByClassName('w-contact-name')[0].innerHTML = _contactName;
+	chatHeader.getElementsByClassName('w-users-messages')[0].innerHTML = _estado;
+	chatHeader.getElementsByTagName('img')[0].src = _imageURL;
+}
+
+function borrarMensajes() {
+    var historyChat = document.getElementById("chat");
+   
+    while (historyChat.firstChild) {
+        historyChat.removeChild(historyChat.firstChild);
     }
 }
-
-
-function crearMensaje(_mensaje)
-{
-var htmlMensajeIn ='<div class="w-message w-message-in">'+'	<div class="w-message-text">'+'h5 class="green-1">Maria Paula Rivarola</h5>'+'<p>Nunca!!! Juan Diego es único</p>'+'<div class="time">11:31</div>'+'</div>'+'</div>';
-
-vard = new Date();
-var htmlMensajeOut='<div class="w-message w-message-out">'+'<div class="w-message-text">'+'<p>'+_mensaje+'</p>'+'<div class="time">  "sd.getHours()"":"d.getMinutes()"</div>'+' </div>'+'</div>';
-    
-var elConversacion= document.getElementById("conversacion");
-    elConversacion.innerHTML += htmlMensajeOut;
-    
-}
-
-function crearChat()
-{
- var elListachats=document.getElementById("lista-chats");
- var lilistItem=document.createElement("LI");
-    
- var htmlChatItem ='<div class="avatar">'+'<img src="image/logocodeacademy.png" alt="" class="wh-44">'+'<h4 class="w-contact-name">Laboratoria Perú</h4>'+'<p class="w-last-message" id="mensaje">No se pique!!</p>'+'</div>'+'<div class="time" id="hora">14:27</div>'
- lilistItem.innerHTML= htmlChatItem;
-
-    elListachats.insertBefore(lilistItem, elListachats.childNodes[0]);
-
-}
-
-function actualizarCabeceraChat()
-{}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
